4f4148b25c8099ebd47384358139e6b188487ee7
Max Bisesi <max.bisesi@gmail.com>
2020-04-17 09:40:04 -0700

    Anagram program complete

--- Anagram.js
+++ Anagram.js
@@ -3,31 +3,38 @@
 // of each other.
 
 const dic = ['Apple','Robot','Teamwork','Asparagus','Glory',
-            'CoffeeBean','Mexico','Notorize','Post','Zuchini'
-            ,'Whipsaw','Treasure','Tandom','Leech']; 
+            'Whipsaw','Treasure','Tandom','Leech','subessential',
+            'suitableness','nectar','recant','tanrec','Caret','carte',
+            'cater','creat','creta','react','adroitley','idolatry','dilatory']; 
 const sigs = [];
 // Sign the dictionary
 const LETTER_MAX = 100;
+
+console.log('Sign each entry\n')
 if(dic.includes(undefined) === true) { throw new Error(); } 
 dic.forEach( (elm) => {
     //turn the word into an array of chars
     if( elm.length > LETTER_MAX){ throw new Error(); }
     
-    let word = elm.toLowerCase().split('');
-    word.sort();
-    const line = `${word.toString()} - ${elm}`;
+    let letters = elm.toLowerCase().split('');
+    letters.sort();
+    let word = '';
+    letters.forEach((char) => { word+=char; });
+    const line = `${word}-${elm}`;
     sigs.push(line);
     console.log(line);
 });
 
-squashed = {};
+console.log('Bring together words of a the same signature.\n');
 
-sigs.forEach( (word) => {
-    let sig = word.split('-');
-    sig = sig[0].trim();
-    console.log(sig); 
-    if( Object.keys(squashed).includes(sig) ) { squashed[sig].push(sig[1]); }
-    else { squashed[sig] = [sig[1]]; }
+squashed = {};
+sigs.forEach( (elem) => {
+    console.log(`===> ${elem}`);
+    let tokens = elem.split('-');
+    console.log(`size = ${tokens.length}`);
+    let sig = tokens[0].trim();
+    if( Object.keys(squashed).includes(sig) ) { squashed[sig].push(tokens[1]); }
+    else { squashed[sig] = [tokens[1]]; }
 });
 
 console.log(JSON.stringify(squashed));
