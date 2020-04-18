44c16a1d7c5c911454ad91d909a298efe9fc76ba
Max Bisesi <max.bisesi@gmail.com>
2020-04-18 12:55:21 -0700

    Date NaN

---
+++ Dates.js
@@ -0,0 +1,43 @@
+// Problem 4 column 3
+/**
+ * Write functions for the following problems
+ * - Given two Dates calculate the number of days between them
+ * - Given a date calculate its day of the week
+ * - Given a month and year produce a calendar for the month as 
+ *      an array of characters.
+ */
+
+ function dateDiff(date1, date2) {
+    const from = validateCreateDate(date1);
+    const to = validateCreateDate(date2);
+    console.log(`From ${from} to ${to}`)
+ }
+
+ // Ensure MM-DD-YYYY format
+ function validateCreateDate(dateVal) {
+    if( typeof dateVal === 'string') {
+        const datenum = [];
+        const tokens = dateVal.split('');
+        if(tokens.length !== 10) { throw new Error(' Use 10 charcters.'); }
+        for(let i = 0; i<10; i++) {
+            let num = tokens[i];
+            if(num === '-' || num === ','){
+                continue;
+            }
+            //num = new Number(num);
+            if(Number(num).isNaN()) {throw new Error(  `${tokens[i]} is not a number. Use a number.` ); }
+            if(Number(num).isInteger() && Number(num).isFinite()) {
+                datenum.push(Number(num));
+                continue;
+            }
+            throw new Error(`Error with ${tokens[i]}`);
+        }
+        return datenum;
+    }
+    throw new Error('use strings');
+ }
+
+let a = process.argv[2];
+let b = process.argv[3];
+
+dateDiff(a,b);
