e6d13e165314292aae3cf7ef4443de4e8324c713
Max Bisesi <max.bisesi@gmail.com>
2020-04-18 11:39:50 -0700

    working on banner

---
+++ Banner.js
@@ -0,0 +1,32 @@
+// Problem 3 Column 3
+/**
+ *  Write a 'Banner' function that recieves a capital letter as input
+ *  and produces as output an array of characters that graphically 
+ *  depicts that letter
+ */
+
+ const border = '************';
+ 
+ const graphLetters = []
+ const A = ` ======
+            ||    || 
+            ||----||
+            ||    || `;
+graphLetters[0] = A;
+
+const B = `=======
+           ||     )
+           ||     )
+           ||====
+           ||     )
+           ||     )
+           =======`
+graphLetters[1] = B;
+
+const C = `  -----
+            /      \\`
+            
+
+ function banner(letter) {
+
+ }
