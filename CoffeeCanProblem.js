1a05668fd71bfe84de7662b9fa5c659fee3f7c7f
Max Bisesi <max.bisesi@gmail.com>
2020-06-09 13:13:44 -0700

    Coffee Can

---
+++ CoffeeCanProblem.js
@@ -0,0 +1,15 @@
+/** 
+ * You are initially given a coffee can that contains some black beans
+ * and some white beans. You are also given a large pile of extra black beans.
+ * You repeat the following procedure until there is one bean left in the can.
+ * 
+ * - Randomly select two beans from the can if they are the same color, throw them
+ *      out and put in an extra black bean.
+ * 
+ * - If they are different colors throw out the black bean and return the white.
+ *      
+ * Prove that the program terminates. What can you say about the color of the last 
+ * bean in the can as a function of the number of black and white beans to befgin with.
+ *  
+ * 
+ */
